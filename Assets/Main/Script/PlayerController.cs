22f5f623bff6e0db3984e8298408921866828067
Masatomo Segawa <masa-segawa@masa-segawa.com>
2016-04-02 14:19:24 +0900

    FirstCommit

--- Assets/Main/Script/PlayerController.cs
+++ Assets/Main/Script/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float movementSpeed = 0.1f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -10,6 +12,22 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		print (Input.GetKeyDown (KeyCode.A));
+
+		Vector3 movement = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.A) == true) {
+		
+			movement = new Vector3 (-movementSpeed, 0.0f, 0.0f);
+
+		}
+
+		if (Input.GetKey (KeyCode.D) == true) {
+
+			movement = new Vector3 (movementSpeed, 0.0f, 0.0f);
+
+		}
+
+		transform.position += movement;
+
 	}
 }
